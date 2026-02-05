ce53a398f4a3a5bdc0e8d2aeae2ee56a22fdcd05
Luiz Felipe <britofelipe85@gmail.com>
2026-02-05 15:24:13 -0300

    Update firstScript.py

--- scripts/firstScript.py
+++ scripts/firstScript.py
@@ -10,3 +10,30 @@ Created on Thu Feb 5
 ---------------------------------------------------------------------------------------------------------------------
 
 """
+
+# Importação de pacotes para usar este script
+import numpy as np
+import matplotlib as pl
+
+def timeSeriesPlot(arr):
+    """
+    
+    Parameters
+    ----------
+    arr : np.array
+        array com a matriz de dados.
+
+    Returns
+    -------
+    fig : TYPE
+        DESCRIPTION.
+        
+    arr_average: numpy array
+
+    Exemplos de uso:
+        fig, arr_average = timeSeriesPlot(arr)
+        
+    """
+    
+    # Média no eixo 1
+    
